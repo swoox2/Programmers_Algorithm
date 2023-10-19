245401df4a14b3b67ef59a894d280ab5b275db7c
Sang Woo <80804697+swoox2@users.noreply.github.com>
2023-10-19 23:53:59 +0900

    Create Sorting_03.java

---
+++ Sorting_03.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int n=citations.length;
+        
+        int h=0;
+        int max=0;
+        while(h<n+1){
+            boolean[] check=new boolean[n];
+            
+            for(int i=0; i<n; i++){
+                if(citations[i]>=h) check[i]=true;
+            }
+            
+            int cnt_t=0;
+            for(int i=0; i<n; i++){
+                if(check[i]) cnt_t++;
+            }
+            
+            if(cnt_t>=h) max=Math.max(max,h);
+            
+            h++;
+        }
+        
+        return max;
+    }
+}
