240a4e59a50dcdf1cc4e77391f354bcc782f99ac
Sang Woo <80804697+swoox2@users.noreply.github.com>
2023-10-27 14:05:10 +0900

    Create 프로세스.java

---
+++ 알고리즘 고득점 Kit/스택과 큐/프로세스.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Node{
+    int idx, prior;
+    
+    Node(int idx, int prior){
+        this.idx=idx;
+        this.prior=prior;
+    }
+    
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Node> q=new LinkedList<>();
+        
+        for(int i=0; i<priorities.length; i++) q.add(new Node(i,priorities[i]));
+        
+        while(!q.isEmpty()){
+            Node cur=q.poll();
+            
+            boolean isAdd=false;
+            for(Node next : q){
+                if(next.prior > cur.prior){
+                    q.add(new Node(cur.idx, cur.prior));
+                    isAdd=true;
+                    break;
+                }
+            }
+            if(isAdd) continue;
+            else{
+                answer++;
+                if(cur.idx==location) break;
+            }
+            
+        
+        }
+        
+        return answer;
+    }
+}
