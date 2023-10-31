087f71b9dfc9a3ec35e07fd1090201023314f350
Sang Woo <wsw960307@gmail.com>
2023-10-31 18:19:48 +0900

    Create 숫자 문자열과 영단어.java

---
+++ Lv1/숫자 문자열과 영단어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] word={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i=0; i<word.length; i++){
+            s=s.replaceAll(word[i],String.valueOf(i));
+        }
+        
+        answer=Integer.parseInt(s);
+
+        return answer;
+    }
+}
