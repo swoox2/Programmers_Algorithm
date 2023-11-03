7b226cf4b57ac1e00467a265e5d5b2a7ca9f3dbc
Sang Woo <wsw960307@gmail.com>
2023-11-03 18:34:18 +0900

    Create 정수 제곱근 판별.java

---
+++ Lv1/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        Double sqrt = Math.sqrt(n);
+        if(sqrt==sqrt.intValue()){
+            answer=(long)Math.pow(sqrt+1,2);
+        }else{
+            answer=-1;
+        }
+        return answer;
+    }
+}
