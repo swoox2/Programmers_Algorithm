dc1d5f1987109078ef72372906df8d6b2394a577
Sang Woo <wsw960307@gmail.com>
2023-11-20 21:45:07 +0900

    Create 멀리뛰기.java

---
+++ Lv2/멀리뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] arr=new long[2000+1];
+        arr[1]=1;
+        arr[2]=2;
+        
+        for(int i=3; i<=n; i++){
+            arr[i]=(arr[i-2]+arr[i-1])%1234567;
+        }
+        
+        answer=arr[n];
+        
+        return answer;
+    }
+}
