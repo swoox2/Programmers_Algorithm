d5d489890b90eca8d17843388fb1135cd3730983
Sang Woo <wsw960307@gmail.com>
2023-11-26 15:13:59 +0900

    Create n^2 배열 자르기.java

---
+++ Lv2/n^2 배열 자르기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left+1)];
+        
+        int idx=0;
+        for(long i=left; i<=right; i++){
+            long row=i/n;
+            long col=i%n;
+            
+            answer[idx++]=Math.max((int)row, (int)col)+1;
+        }
+        return answer;
+    }
+}
