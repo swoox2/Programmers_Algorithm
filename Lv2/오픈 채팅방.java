a91024b7b71500631e3803924b935a98298f950c
Sang Woo <wsw960307@gmail.com>
2023-12-01 00:10:19 +0900

    Update 오픈 채팅방.java

--- Lv2/오픈 채팅방.java
+++ Lv2/오픈 채팅방.java
@@ -9,13 +9,8 @@ class Solution {
             String[] str=record[i].split(" ");
             if(str[0].equals("Leave")) continue;
             
-            String s1=str[0]; //Enter or Leave or Change
-            String s2=str[1]; //유저 아이디
-            String s3=str[2]; //닉네임
-            
-            map.put(s2,s3);
+            map.put(str[1],str[2]);
         }
-        
         
         for(int i=0; i<record.length; i++){
             String[] str=record[i].split(" ");
@@ -25,7 +20,6 @@ class Solution {
             }else if(str[0].equals("Leave")){
                 answer.add(map.get(str[1])+"님이 나갔습니다.");
             }else continue;
-            
             
         }
         
