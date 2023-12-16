e3d5113c497c1000652c9eebdf4268b149383b03
Sang Woo <wsw960307@gmail.com>
2023-12-16 16:06:30 +0900

    Create 크레인 인형뽑기 게임.java

---
+++ Lv1/크레인 인형뽑기 게임.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> stack=new Stack<>();
+        
+        for(int i=0; i<moves.length; i++){
+            int move=moves[i];
+            
+            for(int j=0; j<board.length; j++){
+                if(board[j][move-1]!=0){
+                    if(stack.isEmpty()) stack.push(board[j][move-1]);
+                    else{
+                        if(stack.peek()==board[j][move-1]){
+                            stack.pop();
+                            answer+=2;
+                        }else stack.push(board[j][move-1]);
+                    }
+                board[j][move-1]=0;
+                break;
+                } 
+            }
+        }
+        
+        return answer;
+    }
+}
